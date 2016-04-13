647764dad96ed58366f18707d4e6ed53c6f1ecd0
gcrowder <gregory.j.crowder@gmail.com>
2016-04-13 17:23:25 -0400

    Refactored a bit and found where I was double adding. Game now works properly with dumb parent Player.

--- pig.py
+++ pig.py
@@ -3,9 +3,8 @@ import random
 
 class Player:
     def __init__(self):
-        self.strategy = ['roll', 'hold']
         self.total = 0
-        self.turn = 0
+        self.turn_amount = 0
 
     def choice(self, turn):
         if self.turn == turn:
@@ -14,37 +13,44 @@ class Player:
             self.turn = turn
             return self.strategy[0]
 
-    def add_to_total(self, number):
-        self.total += number
+    # def add_to_total(self, number):
+    #     self.total += number
 
-    def next_turn(self):
-        self.turn += 1
+    def is_roll_again(self):
+        return False
 
 
 def roll_die():
     return random.randint(1, 6)
 
 
-def play(player, turn):
+def play(player):
     total = player.total
-    choice = player.choice(turn)
-    if choice == 'roll':
-        roll = roll_die()
-        if roll == 1:
-            return total
-        else:
-            total += roll
-            return total
+    roll = roll_die()
+    if roll == 1:
+        return total
     else:
+        total += roll
         return total
 
 
 def game_loop(player):
     result = 0
     for turn in range(7):
-        result = play(player, turn)
-        while player.total != result:
-            player.add_to_total(result)
-            result = play(player, turn)
-            player.next_turn()
+        result = play(player)
+        player.total = result
+        while player.is_roll_again():
+            result = play(player)
+            player.total = result
     return player.total
+
+
+def main():
+    player_class_trials = []
+    for _ in range(1):
+        bob = Player()
+        player_class_trials.append(game_loop(bob))
+    print("List: ", player_class_trials)
+
+if __name__ == '__main__':
+    main()
--- test_pig.py
+++ test_pig.py
@@ -2,9 +2,9 @@ from pig import *
 from unittest import mock
 
 
-def test_create_player_rolls_holds():
-    bob = Player()
-    assert bob.strategy == ['roll', 'hold']
+# def test_create_player_rolls_holds():
+#     bob = Player()
+#     assert bob.strategy == ['roll', 'hold']
 
 
 def test_create_player_total_zero():
@@ -12,26 +12,26 @@ def test_create_player_total_zero():
     assert bill.total == 0
 
 
-def test_add_to_player_total():
-    jeff = Player()
-    jeff.add_to_total(1)
-    assert jeff.total == 1
+# def test_add_to_player_total():
+#     jeff = Player()
+#     jeff.add_to_total(1)
+#     assert jeff.total == 1
 
 
-def test_next_player_turn():
-    sally = Player()
-    sally.next_turn()
-    assert sally.turn == 1
+# def test_next_player_turn():
+#     sally = Player()
+#     sally.next_turn()
+#     assert sally.turn == 1
 
 anne = Player()
 
 
-def test_player_choice_roll():
-    assert anne.choice(1) == 'roll'
-
-
-def test_player_choice_hold():
-    assert anne.choice(1) == 'hold'
+# def test_player_choice_roll():
+#     assert anne.choice(1) == 'roll'
+#
+#
+# def test_player_choice_hold():
+#     assert anne.choice(1) == 'hold'
 
 
 @mock.patch('random.randint')
@@ -44,7 +44,7 @@ def test_roll(mock_random):
 def test_turn(mock_random):
     mock_random.return_value = 6
     joe = Player()
-    assert play(joe, 1) == 6
+    assert play(joe) == 6
 
 
 @mock.patch('random.randint')
