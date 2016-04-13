76c71fa3421e3bab4bdacbc176f87290b5ae289d
gcrowder <gregory.j.crowder@gmail.com>
2016-04-13 13:47:38 -0400

    Tested, wrote and tested roll().

--- pig.py
+++ pig.py
@@ -1,3 +1,10 @@
+import random
+
+
 class Player:
     def __init__(self):
         self.strategy = ['roll', 'hold']
+
+
+def roll():
+    return random.randint(1, 6)
--- test_pig.py
+++ test_pig.py
@@ -1,6 +1,13 @@
-from pig import Player
+from pig import *
+from unittest import mock
 
 
 def test_create_player_rolls_holds():
     bob = Player()
     assert bob.strategy == ['roll', 'hold']
+
+
+@mock.patch('random.randint')
+def test_roll(mock_random):
+    mock_random.return_value = 6
+    assert roll() == 6
