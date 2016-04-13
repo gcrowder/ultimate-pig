0f64a2f7f03cac0d25fdc8dce8087b44c16f9676
gcrowder <gregory.j.crowder@gmail.com>
2016-04-13 14:16:22 -0400

    Wrote tests and functions and attributes.

--- pig.py
+++ pig.py
@@ -4,7 +4,25 @@ import random
 class Player:
     def __init__(self):
         self.strategy = ['roll', 'hold']
+        self.total = 0
+
+    def choice(self):
+        return self.strategy[0]
 
 
-def roll():
+def roll_die():
     return random.randint(1, 6)
+
+
+def turn(player):
+    total = player.total
+    choice = player.choice()
+    if choice == 'roll':
+        roll = roll_die()
+        if roll == 1:
+            return total
+        else:
+            total += roll
+            return total
+    else:
+        return total
--- test_pig.py
+++ test_pig.py
@@ -7,7 +7,19 @@ def test_create_player_rolls_holds():
     assert bob.strategy == ['roll', 'hold']
 
 
+def test_create_player_total_zero():
+    bill = Player()
+    assert bill.total == 0
+
+
 @mock.patch('random.randint')
 def test_roll(mock_random):
     mock_random.return_value = 6
-    assert roll() == 6
+    assert roll_die() == 6
+
+
+@mock.patch('random.randint')
+def test_turn(mock_random):
+    mock_random.return_value = 6
+    joe = Player()
+    assert turn(joe) == 6
